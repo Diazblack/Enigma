1e55483b5e53c754f0bfd4c3fe9231e646ffc65b
Aizar <aizar.cabrera@icloud.com>
2018-08-25 12:37:48 -0600

    Create class and attributes

---
+++ test/enigma_test.rb
@@ -0,0 +1,63 @@
+require 'simplecov'
+SimpleCov.start
+
+# Previous content of test helper now starts here
+
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require 'date'
+
+class EnigmaTest < Minitest::Test
+
+  def test_if_it_exists
+    e = Enigma.new
+
+    assert_instance_of Enigma, e
+  end
+
+  def test_if_it_has_attributes
+    e = Enigma.new
+
+    expected =["a" => ".-",
+                    "b" => "-...",
+                    "c" => "-.-.",
+                    "d" => "-..",
+                    "e" => ".",
+                    "f" => "..-.",
+                    "g" => "--.",
+                    "h" => "....",
+                    "i" => "..",
+                    "j" => ".---",
+                    "k" => "-.-",
+                    "l" => ".-..",
+                    "m" => "--",
+                    "n" => "-.",
+                    "o" => "---",
+                    "p" => ".--.",
+                    "q" => "--.-",
+                    "r" => ".-.",
+                    "s" => "...",
+                    "t" => "-",
+                    "u" => "..-",
+                    "v" => "...-",
+                    "w" => ".--",
+                    "x" => "-..-",
+                    "y" => "-.--",
+                    "z" => "--..",
+                    "1" => ".----",
+                    "2" => "..---",
+                    "3" => "...--",
+                    "4" => "....-",
+                    "5" => ".....",
+                    "6" => "-....",
+                    "7" => "--...",
+                    "8" => "---..",
+                    "9" => "----.",
+                    "0" => "-----",
+                    " " => " "]
+    assert_equal expected, e.dictionary
+  end
+
+end
